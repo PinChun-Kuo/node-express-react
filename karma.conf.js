d9016e1508787ce6de0bd75ddbb63c4130e1af97
Kuo <pinchun.kuo@rakuten.com>
2016-11-16 14:09:14 +0800

    combine karma with test

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,60 @@
 // karma.conf.js
 module.exports = function(config) {
-  config.set({
-    frameworks: ['mocha'],
+    config.set({
+        basePath: './',
 
-    files: [
-      './public/javascripts/*.js'
-    ],
+        // List of test frameworks you want to use. Typically, you will set this to ['jasmine'], ['mocha'] or ['qunit']...
+        frameworks: ['mocha'],
 
-    client: {
-      mocha: {
-        // change Karma's debug.html to the mocha web reporter
-        reporter: 'html',
+        // A list of browsers to launch and capture.
+        browsers: ['Chrome', 'PhantomJS'],
 
-        // require specific files after Mocha is initialized
-        require: [require.resolve('bdd-lazy-var/bdd_lazy_var_global')],
+        //  How long will Karma wait for a message from a browser before disconnecting from it (in ms).
+        browserNoActivityTimeout: 30000,
 
-        // custom ui, defined in required file above
-        ui: 'bdd-lazy-var/global',
-      }
-    }
-  });
+        // List of files/patterns to load in the browser.
+        files: [
+            './test/test.js'
+        ],
+
+        // List of plugins to load. A plugin can be a string (in which case it will be required by Karma) or an inlined plugin - Object.
+        plugins: [
+            'karma-mocha',
+            'karma-mocha-reporter',
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher',
+            'karma-webpack'
+        ],
+
+        // Preprocessors in Karma allow you to do some work with your files before they get served to the browser.
+        // These are configured in the preprocessors block of the configuration file:
+        preprocessors: {
+            './test/test.js': ['webpack'],
+        },
+
+        webpack: require('./webpack.config.js'),
+
+        // A list of reporters to use.
+        reporters: ['mocha'],
+
+        // The port where the web server will be listening.
+        port: 9876,
+
+        // Enable or disable colors in the output (reporters and logs).
+        colors: true,
+
+        // Level of logging.
+        logLevel: config.LOG_INFO,
+
+        // test 監聽
+        autoWatch: false,
+
+        // Continuous Integration mode.
+        // If true, Karma will start and capture all configured browsers, run tests
+        // and then exit with an exit code of 0 or 1 depending on whether all tests passed or any tests failed.
+        singleRun: true,
+
+        // How many browsers Karma launches in parallel.
+        concurrency: Infinity
+    });
 };
--- test/test.js
+++ test/test.js
@@ -3,20 +3,6 @@ import assert from 'assert';
 import ReactTestUtils from 'react-addons-test-utils';
 import Component from '../public/javascripts/helloWorld.js';
 
-describe('Array', function() {
-    describe('indexOf()', function() {
-        it('should return -1 when the value is not present', function(done) {
-            assert.equal(-1, [1, 2, 3, 4].indexOf(5));
-            done();
-        })
-
-        it('should return -12 when the value is not present', function(done) {
-            assert.equal(-1, [1, 2, 3, 4].indexOf(0));
-            done();
-        })
-    })
-});
-
 describe('The root div', () => {
     it('should contain text: Hello, World!', done => {
         const renderer = ReactTestUtils.createRenderer();
@@ -25,11 +11,7 @@ describe('The root div', () => {
             React.createElement(Component)
         );
 
-        // const output = renderer.getRenderOutput().props.children;
         const content = renderer.getRenderOutput().props.children.props.children;
-
-        // console.log("-----output----- : ", output);
-        // console.log("-----content----- : ", content);
 
         assert.equal(content, 'Hello, World!');
         done();
