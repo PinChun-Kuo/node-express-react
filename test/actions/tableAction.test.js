ae9aea4e35402b5d0d0a63620ac47d96f831fd91
Kuo <pinchun.kuo@rakuten.com>
2016-12-09 09:22:45 +0800

    modify statement for action test

--- test/actions/tableAction.test.js
+++ test/actions/tableAction.test.js
@@ -14,31 +14,31 @@ const seq = 1;
 
 describe('public/javascripts/actions/tableAction.js Spec', () => {
   describe('Actions output', () => {
-    it('Should return an object which type is add item when running addItemAction.', () => {
+    it('Should handle add item.', () => {
       should.exist(tableAction.addItemAction(dataRow));
       tableAction.addItemAction(dataRow).type.should.be.equal(tableAction.tableActionaType.addItem);
       tableAction.addItemAction(dataRow).payload.addRow.should.be.equal(dataRow);
     });
 
-    it('Should return an object which type is edit item when running editItemAction.', () => {
+    it('Should handle edit item.', () => {
       should.exist(tableAction.editItemAction(dataRow));
       tableAction.editItemAction(dataRow).type.should.be.equal(tableAction.tableActionaType.editItem);
       tableAction.editItemAction(dataRow).payload.editRow.should.be.equal(dataRow);
     });
 
-    it('Should return an object which type is delete item when running deleteItemAction.', () => {
+    it('Should handle delete item.', () => {
       should.exist(tableAction.deleteItemAction(dataRow));
       tableAction.deleteItemAction(seq).type.should.be.equal(tableAction.tableActionaType.deleteItem);
       tableAction.deleteItemAction(seq).payload.seq.should.be.equal(seq);
     });
 
-    it('Should return an object which type is open pop modal when running openPopModal.', () => {
+    it('Should handle open pop modal.', () => {
       should.exist(tableAction.openPopModal(dataRow));
       tableAction.openPopModal(dataRow).type.should.be.equal(tableAction.tableActionaType.openPopModal);
       tableAction.openPopModal(dataRow).payload.editRow.should.be.equal(dataRow);
     });
 
-    it('Should return an object which type is close pop modal when running closePopModal.', () => {
+    it('Should handle close pop modal.', () => {
       should.exist(tableAction.closePopModal());
       tableAction.closePopModal().type.should.be.equal(tableAction.tableActionaType.closePopModal);
     });
