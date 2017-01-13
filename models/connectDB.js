9a4c68fa6c773e158f9c3b58329b742a97b3f1b5
PinChun-Kuo <pinery201@gmail.com>
2017-01-13 14:00:12 +0800

    modify the method for connecting DB

--- models/connectDB.js
+++ models/connectDB.js
@@ -1,15 +1,21 @@
 var sequelize = require('./DB.config')
 
-if(process.env.NODE_ENV === 'production') {
-  var connection = sequelize.sync({
-    force: true
-  });
-} else {
-  var connection = sequelize.authenticate();
-}
+// if(process.env.NODE_ENV === 'production') {
+//   var connection = sequelize.sync({
+//     force: true
+//   });
+// } else {
+//   var connection = sequelize.authenticate();
+// }
 
-connection.then(function() {
+// connection.then( function() {
+//   console.log('\nConnection has been established successfully.');
+// }).catch( function(error) {
+//   console.log('\nUnable to connect to the database:', error);
+// });
+
+sequelize.authenticate().then( function() {
   console.log('\nConnection has been established successfully.');
-}).catch(function(error) {
+}).catch( function(error) {
   console.log('\nUnable to connect to the database:', error);
 });
