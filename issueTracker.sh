db19448b6044bb40692b86df5de6728dc30dd809
PinChun-Kuo <pinery201@gmail.com>
2017-01-17 16:47:25 +0800

    modify scripts command in packahe.json

--- issueTracker.sh
+++ issueTracker.sh
@@ -2,13 +2,13 @@
 docker pull postgres
 
 # create postgres container
-docker run --name some-postgres -e POSTGRES_PASSWORD=12345678 -d postgres
+docker run --name issue-postgres -e POSTGRES_PASSWORD=12345678 -d postgres
 
 # create issue_tracker container
 docker build -t chloe/issue_tracker .
 
 # link both of them and create database by postgres role first
-docker run -P --name issue_tracker -p 3000:3000 --link some-postgres:postgres -e POSTGRES_DB=issueTracker -d chloe/issue_tracker
+docker run -P --name issue_tracker -p 3000:3000 --link issue-postgres:postgres -e POSTGRES_DB=issueTracker -d chloe/issue_tracker
 
 #  build code
-docker exec -it issue_tracker /bin/bash -c "npm run build"
+docker exec -it issue_tracker /bin/bash -c "npm run start"
